bd630c38aee61aa445a01e19861ce09687309e44
lovesykkkk <lovesykim@iei.or.kr>
2022-10-31 01:02:27 +0900

    array
    array

--- 04_Array/src/com/kh/array/B_ArrayCopy.java
+++ 04_Array/src/com/kh/array/B_ArrayCopy.java
@@ -1,5 +1,8 @@
 package com.kh.array;
 
+import java.util.Arrays;
+//import java.lang; => 알게모르게 포함되어있음
+
 public class B_ArrayCopy {
 
 	// 배열 복사
@@ -80,6 +83,86 @@ public class B_ArrayCopy {
 			System.out.print(copy[i] + " ");
 		}
 		System.out.println();
+		
+		System.out.println("origin주소값 : " + origin.hashCode());
+		System.out.println("copy주소값 : " + copy.hashCode());
+	}
+	
+	public void method3() {
+		// 2. 아싸리 새로운 배열 생성후 System이라는 클래스에서의 arraycopy() 메소드를 이용한 복사
+		int[] origin = {1,2,3,4,5};
+		int[] copy = new int[10]; // 넉넉하게 크게 생성 / 0~9 인덱스까지 0이 다 담겨있을것!!
+		
+		//System.arraycopy(원본배열명, 복사시작할인덱스, 복사본배열명, 복사본배열의 복사될 시작인덱스, 복사할갯수);
+		
+		//System.arraycopy(origin, 0, copy, 0, 5);
+		//System.arraycopy(origin, 0, copy, 2, 5);
+		System.arraycopy(origin, 1, copy, 3, 3);
+		
+		for(int i =0; i<copy.length; i++) {
+			System.out.print(copy[i] + " ");
+		}
+		System.out.println();
+		System.out.println("origin주소값 : " + origin.hashCode());
+		System.out.println("copy주소값 : " + copy.hashCode());
+		//주소값이 다름(깊은복사)
+		// => 각자 다른 배열 참조하고 있음
+		// => 즉, 배열 수정시 서로에게 영향을 주지 않음
+		
+		
+	}
+	
+	public void method4() {
+		//3. Arrays 클래스에서 제공하는 copyOf() 메소드 사용
+		int[] origin = {1,2,3,4,5};
+		// 복사본 배열 = Arrays.copyOf(원본배열명, 복사할길이);
+		//int []copy = Arrays.copyOf(origin, 5);
+		//int []copy = Arrays.copyOf(origin, 3);
+		int []copy = Arrays.copyOf(origin, 10);
+		
+		/*
+		 * [참고]
+		 * java.lang.Math
+		 * java.lang.String
+		 * java.lang.System
+		 * => java.lang 패키지에 있는 클래스들을 쓸 때는 import안해도됨!
+		 * 
+		 * java.util.Scanner
+		 * java.util.Arrays
+		 * => java.lang 이외의 패키지에 있는 클래스들을 쓸 때는 무조건 import 해야됨!!
+		 * 
+		 */
+		
+		for(int i=0; i<copy.length; i++) {
+			System.out.print(copy[i] + " ");
+		}
+		System.out.println();
+		
+		/*
+		 * Arrays.copyOf 메소드
+		 * => 내가 제시한 길이 만큼 복사본 배열 크기 할당
+		 * => 단, 내가 제시한 길이가 원본배열의 크기보다 클 경우 기존의 배열에 있는값은 다 복사나머지는 0으로 채운다.
+		 * => 	  내가 제시한 길이가 원본배열의 크기보다 작을 경우 기존의 배열에 있는 값 내가 제시한 길이만큼만 복사.
+		 */
+		
+		System.out.println("origin 주소값 : "+origin.hashCode());
+		System.out.println("copy 주소값 : "+copy.hashCode());
+	}
+	
+	public void method5() {
+		//4. clone() 메소드 이용한 복사
+		//	 인덱스 지정불가, 크기 지정 별도로 못함
+		int[] origin = {1,2,3,4,5};
+		// 복사본 배열 = 원본배열.clone();
+		int[] copy = origin.clone();
+		
+		for(int i =0; i<copy.length; i++) {
+			System.out.print(copy[i] + " ");
+		}
+		System.out.println();
+		
+		System.out.println("origin 주소값 : "+origin.hashCode());
+		System.out.println("copy 주소값 : "+copy.hashCode());
 		
 		
 	}
--- 04_Array/src/com/kh/run/ArrayRun.java
+++ 04_Array/src/com/kh/run/ArrayRun.java
@@ -21,7 +21,10 @@ public class ArrayRun {
 		
 		B_ArrayCopy b = new B_ArrayCopy();
 		//b.method1();
-		b.method2();
+		//b.method2();
+		//b.method3();
+		//b.method4();
+		b.method5();
 		
 		
 	}
